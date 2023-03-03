c16e9f2faa656d5fcdb40dd86873ea8bbc479cab
ontgrid <yu.zhang@simceredx.com>
2023-03-03 10:02:57 +0800

    feat: 2023.3.3

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import jsPDF from "jspdf";
 
 import styles from "./App.module.less";
 
-const {
-  userBasicInfo,
-  projectExperience,
-  educationExperience
-} = window.globalData;
+const { userBasicInfo, projectExperience, educationExperience } =
+  window.globalData;
 class App extends Component {
   download = () => {
-    html2canvas(this.refs.pdf, { scale: 2 }).then(canvas => {
+    html2canvas(this.refs.pdf, { scale: 2 }).then((canvas) => {
       //返回图片dataURL，参数：图片格式和清晰度(0-1)
       let pageData = canvas.toDataURL("image/jpeg", 1.0);
 
       let dims = {
         a2: [1190.55, 1683.78],
         a3: [841.89, 1190.55],
-        a4: [595.28, 841.89]
+        a4: [595.28, 841.89],
       };
       //方向默认竖直，尺寸ponits，格式a2
       let pdf = new jsPDF("", "pt", "a4");
@@ -107,22 +104,27 @@ class App extends Component {
               </div>
               <Col span={20} offset={4} style={{ paddingLeft: 10 }}>
                 <p className={styles.skill}>
-                  4年 react /2年react native 开发经验:
+                  5年 react /3年react native 开发经验:
                 </p>
                 <p className={styles.skillInfo}>
-                应用:react-redux
+                  应用:react-redux
                   、reudx-saga、webpack、antdesign、lodash、graphql、axios、
-                  tailwindcss 等第三方库;
+                  tailwindcss，uncoss 等第三方库;
                 </p>
                 <p className={styles.skillInfo}>
-                熟练 flex 布局，组件化，性能优化,Typescript,eslint,husky,vite,webpack 等;
+                  flex
+                  布局，组件化，性能优化,Typescript,eslint,husky,vite,webpack
+                  等;
                 </p>
+                <p className={styles.skill}>4年 vue 开发经验:</p>
+                <p className={styles.skillInfo}>
+                
+                  使用pinia，vue-router,element等;
+                </p>
+                <p className={styles.skill}>使用docker，docker-compose，部署mysql，redis，koajs服务、nginx等;</p>
                 <p className={styles.skill}>
-                3年 vue 开发经验:
+                   搭建公司npm命令，搭建UI组件和业务组件库，搭建项目，分配任务等
                 </p>
-                <p className={styles.skillInfo}> 使用pinia，vue-router,antd-vue等;</p>
-                <p className={styles.skill}>学习 dart flutter,docker,jenkins,nginx,nodejs等;</p>
-                <p className={styles.skill}>熟练使用 git 命令;</p>
               </Col>
             </Row>
             <Row style={{ marginTop: 20 }}>
@@ -189,7 +191,7 @@ class App extends Component {
                         {w}
                       </p>
                     ))}
-                    <p>解决问题:</p>
+                    <p>描述:</p>
                     {work.question.map((w, j) => (
                       <p className={styles.pSubinfo} key={j}>
                         {w}
@@ -211,6 +213,36 @@ class App extends Component {
                   </div>
                 </div>
               </div>
+            </Row>
+            <Row style={{ marginTop: 20, textAlign: "left" }}>
+            <div
+                className={styles.workExperience}
+                style={{ marginBottom: 15 }}
+              >
+                <div className={`${styles.workInfo} `}>
+                  <div className={styles.workYear} />
+                  <div className={`${styles.workCompany} ${styles.diamondBox}`}>
+                    <span className={styles.diamondIcon} /> ont一体机
+                  </div>
+                </div>
+                <div className={styles.workDuty}>
+                  <p>负责功能:</p>
+
+                  <p className={styles.pSubinfo} >
+                  整个项目的开发和维护，使用koajs开发服务端读取设备检验数据，vue开发客户端展示结果等
+                  </p>
+                  <p>描述:</p>
+                  <p className={styles.pSubinfo}>
+                  1、使用docker，docker-conpose实现在服务器和一体机命令部署和自动重启服务，部署redis和mysql、nginx，server端服务，
+                  </p>
+                  <p className={styles.pSubinfo}>
+                  2、解决了nodejs中使用pdf，使用wkhtmltopdf库生成pdf，使用docker下载wkhtmltopdf，下载规定字体并导入一体机字体库中
+                  </p>
+                  <p className={styles.pSubinfo}>
+                  3、volumes使容器和本地之前共享错误日志和输出日志，和数据持久化
+                  </p>
+                </div>
+              </div>
               <div
                 className={styles.workExperience}
                 style={{ marginBottom: 15 }}
@@ -218,21 +250,27 @@ class App extends Component {
                 <div className={`${styles.workInfo} `}>
                   <div className={styles.workYear} />
                   <div className={`${styles.workCompany} ${styles.diamondBox}`}>
-                    <span className={styles.diamondIcon} /> 预约助手小程序
+                    <span className={styles.diamondIcon} /> 小云鹤微信公众号
                   </div>
                 </div>
                 <div className={styles.workDuty}>
                   <p>负责功能:</p>
 
-                  <p className={styles.pSubinfo}>使用微信开发工具开发小程序</p>
-                  <p>解决问题:</p>
-
-                  <p className={styles.pSubinfo}>1、长列表交互使用key</p>
-                  <p className={styles.pSubinfo}>2、setData赋值优化</p>
+                  <p className={styles.pSubinfo} >
+                  整个公众号的开发维护，使用了react，redux，react-loadable等
+                  </p>
+                  <p>描述:</p>
+                  <p className={styles.pSubinfo}>
+                  1、使用webpack4，antd-mobile，redux，axios搭建的框架，
+                  </p>
+                  <p className={styles.pSubinfo}>
+                  2、使用react-loadable实现代码分割，
+                  </p>
+                  <p className={styles.pSubinfo}>
+                  3、通过react-tiny-virtual-list来优化长列表，
+                  </p>
                 </div>
               </div>
-            </Row>
-            <Row style={{ marginTop: 20, textAlign: "left" }}>
               <div className={styles.titleBox}>
                 <div className={styles.titleHeader}> 教育经历</div>
                 <div className={styles.lineFlex}>
@@ -279,11 +317,7 @@ class App extends Component {
                   <div className={styles.workYear} />
                   <div className={`${styles.workCompany} `}>
                     <p>Github:https://github.com/weizheng1992</p>
-                    <p />
-                    <p>github 有vite和webpack5搭建的框架;</p>
-                    <p>vite-ts-react使用的新技术vite2，reacthooks,immer,router6.0,antd,ts,redux搭建的后台管理</p>
-                    <p>react-node 使用nodejs，express,ts,jsonwebtoken,搭建的简易接口</p>
-                    <p>线上地址：http://8.142.136.225/ admin/123456 使用docker，jenkins，nginx等搭建</p>
+                    <p>掘金:https://juejin.cn/user/2999123452376711</p>
                   </div>
                 </div>
               </div>
